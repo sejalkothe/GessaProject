d33cf3f4b7f17cc9783f9548b0d338bd4ece6e31
Neha Mishra <neha.mishra@iauro.com>
2022-06-24 14:41:01 +0530

    SLice iNtegration

--- apps/pages-gessa/src/app/layouts/AppMenu/AppMenu.tsx
+++ apps/pages-gessa/src/app/layouts/AppMenu/AppMenu.tsx
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { List } from '@mui/material';
 import AppMenuItem from './AppMenuItem';
 import NavMenuItem from './NavMenuItem';
 import { appMenuItems } from './app-menu-items';
+import { useAppDispatch } from '../../../context/redux';
+import { getAppMenu } from '../../pages/projects/store/appMenuSlice';
 
 interface Props {
   menuType: string;
 }
 
 function AppMenu({ menuType }: Props) {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(getAppMenu({ page: 1, size: 8 }))
+      .then((data) => {
+        console.log('data', data);
+      })
+      .catch((reason) => {
+        console.log('reason', reason);
+      });
+  });
+
+  // const handleMenu = () => {
+  //   dispatch(getAppMenu({ page: 1, size: 8 }))
+  //     .unwrap()
+  //     .then()
+  //     .catch((reason) => {
+  //       console.log('reason', reason);
+  //     });
+  // };
+
   return (
     <div>
       {menuType === 'classic' ? (
---
+++ apps/pages-gessa/src/app/pages/projects/store/appMenuSlice.ts
@@ -0,0 +1,52 @@
+import {
+  createSlice,
+  createEntityAdapter,
+  createAsyncThunk,
+  createSelector,
+  EntityState,
+} from '@reduxjs/toolkit';
+import { IRootState } from '../../../../store';
+import axios from 'axios';
+
+type _IRGridData = EntityState<IRGrid>;
+export interface IRGridData extends _IRGridData {
+  activePageId: string;
+}
+export interface IRGrid extends _IRGridData {
+  id: string;
+  page: string;
+  data: any;
+}
+
+export const getAppMenu = createAsyncThunk(
+  'features',
+  async (menuContent: any, { dispatch }) => {
+    const response: any = await axios.get(
+      `http://gessa.io/rbac/features`,
+      menuContent.data
+    );
+    debugger;
+    const data: any = response.data.result.data;
+    dispatch(setMenus(data));
+    //   dispatch(setNodes(data[0].nodes));
+    //   dispatch(setChart(data[0].flowchart));
+  }
+);
+
+const appMenuAdapter = createEntityAdapter<IRGrid>({
+  selectId: ({ id }) => id,
+});
+export const { selectAll: selectAllMenu } = appMenuAdapter.getSelectors(
+  (state: any) => state.menuListSlice
+);
+
+const menuListSlice = createSlice({
+  name: 'appMenu',
+  initialState: appMenuAdapter.getInitialState({}),
+  reducers: {
+    setMenus: appMenuAdapter.setAll,
+  },
+});
+
+export const { setMenus } = menuListSlice.actions;
+export default menuListSlice.reducer;
