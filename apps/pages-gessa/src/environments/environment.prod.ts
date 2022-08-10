cbfef05c0c5680386e9fdee8f2bd6ace3e221eb5
Pravinkumar Borate <pravin.borate@iauro.com>
2022-08-10 20:11:26 +0530

    added https to NX_THEME_BASE_URL

--- apps/pages-gessa/src/environments/environment.prod.ts
+++ apps/pages-gessa/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  NX_THEME_BASE_URL: 'http://gessa.io/gessa-project',
+  NX_THEME_BASE_URL: 'https://gessa.io/gessa-project',
   NX_FEATURE_BASE_URL: 'https://gessa.io/rbac',
   NX_KEYCLOCK_AUTH_SERVER_URL: 'https://auth-dev.gessa.io',
   NX_KEYCLOCK_DATABASE: 'gessa-studio',
