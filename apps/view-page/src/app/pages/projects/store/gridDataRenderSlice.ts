5f9a1031c252185c37ef7014002342b5356a20a5
vishal.kokane <vishal.kokane@iauro.com>
2023-01-17 10:22:40 +0530

    share changes pushed

--- apps/view-page/src/app/pages/projects/store/gridDataRenderSlice.ts
+++ apps/view-page/src/app/pages/projects/store/gridDataRenderSlice.ts
@@ -30,11 +30,11 @@ export interface IDataResourseParams {
 export const getChartDataResource = createAsyncThunk(
   'get chart resource',
   async (params: IDataResourseParams, { dispatch }) => {
-    const response: any = {};
-    // const response: any = await axios.get(
-    //   environment.NX_SCHEMA_SERVICE +
-    //     `/chart-data/report/${params.report}/label/${params.label}`
-    // );
+    // const response: any = {};
+    const response: any = await axios.get(
+      environment.NX_SCHEMA_SERVICE +
+        `/chart-data/report/${params.report}/label/${params.label}`
+    );
     if (response && response.data) {
       const data: IWidgetDataRender = response.data;
       const payload: IWidgetDataRender = {
@@ -90,7 +90,7 @@ export const getChartDataResource = createAsyncThunk(
         widgetid: params.widget_id,
       };
       // dispatch(setGridDataRender(payload));
-      return myData;
+      // return myData;
     }
     return response;
   }
